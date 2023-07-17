6d43e8058cb7bf630c5a91b40c99881796a1cc56
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-07-17 11:35:03 +0300

    add button to landing

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -4,9 +4,11 @@ import 'package:google_fonts/google_fonts.dart';
 abstract class Styles{
   static const textStyle24 = TextStyle(
     fontSize: 24,
+    color: Color(0xff3C2513)
   );
 
   static const textStyle20 = TextStyle(
     fontSize: 20,
+    color: Color(0xff3C2513)
   );
 }
--- lib/features/splash/presentation/widgets/landing_view_body.dart
+++ lib/features/splash/presentation/widgets/landing_view_body.dart
@@ -1,15 +1,38 @@
 import 'package:coffee_app/core/utils/assets.dart';
+import 'package:coffee_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+
 class LandingViewBody extends StatelessWidget {
   const LandingViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final size=MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return Stack(
-
       children: [
-        Image.asset(AppAssets.kLandingImage,width: size.width,height: size.height,fit: BoxFit.cover,),
+        Image.asset(
+          AppAssets.kLandingImage,
+          width: size.width,
+          height: size.height,
+          fit: BoxFit.cover,
+        ),
+        Positioned(
+            bottom: 10,
+          left: size.height * 0.03,
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: const Color(0xFFD1D0D7),
+                borderRadius: BorderRadius.circular(32),
+                  boxShadow: const [
+                    BoxShadow(color: Color(0xff1B1B1B), spreadRadius: 2,)
+                  ]
+              ),
+
+              width: size.width*0.9,
+              height: size.height*0.083,
+              child: const Text('data',style: Styles.textStyle20,),
+            )),
       ],
     );
   }
